15f6ef84886f66967f1e02e69f0f05d5c84a75a4
gchagan36 <gchagan36@gmail.com>
2022-01-06 22:57:45 -0500

    Javascript from Lectures 47-51

---
+++ html-css-javascript-for-web-devs/module4/js/Lecture49.js
@@ -0,0 +1,15 @@
+// Object literals and "this"
+console.log("\nLecture 49:")
+var literalCircle = { // new Object()
+    radius: 10,
+    getArea: function () {
+        var self = this;
+        console.log(this);
+        var increaseRadius = function () {
+            self.radius = 20;
+        }
+        increaseRadius();
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+};
+console.log(literalCircle.getArea());
---
+++ html-css-javascript-for-web-devs/module4/js/Lecture50.js
@@ -0,0 +1,43 @@
+// Arrays
+console.log("\nLecture 50:");
+var array = new Array();
+array[0] = "Yaakov";
+array[1] = 2;
+array[2] = function (name) {
+  console.log("Hello " + name);
+};
+array[3] = {
+    course: "HTML, CSS, & JS"
+};
+console.log(array);
+console.log(array[1]);
+array[2](array[0]);
+console.log(array[3].course);
+
+var names = ["Yaakov", "John", "Joe"];
+console.log(names);
+for (var i=0; i<names.length; i++) {
+  console.log("Hello " + names[i]);
+};
+names[100] = "Jim";
+for (var i=0; i<names.length; i++) {
+    console.log("Hello " + names[i]);
+};
+
+var names2 = ["Yaakov", "John", "Joe"];
+var myObj = {
+    name: "Yaakov",
+    course: "HTML/CSS/JS",
+    platform: "Coursera"
+};
+for (var prop in myObj) {
+  console.log(prop + ": " + myObj[prop]);
+};
+
+for (var name in names2) {
+    console.log("Hello " + names2[name])
+}
+names2.greeting = "Hi!";
+for (var name in names2) {
+    console.log("Hello " + names2[name])
+}
